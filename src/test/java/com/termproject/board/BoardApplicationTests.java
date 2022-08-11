297ffbbac05daadf46cc5540e5e17fa4b4ecee08
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-08-11 18:45:16 +0900

    CI/CD 구축완료

--- src/test/java/com/termproject/board/BoardApplicationTests.java
+++ src/test/java/com/termproject/board/BoardApplicationTests.java
@@ -2,7 +2,9 @@ package com.termproject.board;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@WebAppConfiguration
 @SpringBootTest
 class BoardApplicationTests {
 
