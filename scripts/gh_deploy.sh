9e84cf063374ac909d34a7f873c794b2caface46
JangDongHa <94433852+JangDongHa@users.noreply.github.com>
2022-08-11 19:19:26 +0900

    Update gh_deploy.sh

--- scripts/gh_deploy.sh
+++ scripts/gh_deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 PROJECT_NAME="github_action"
-JAR_PATH="/home/ubuntu/github_action/build/libs/*.jar"
-DEPLOY_PATH=/home/ubuntu/$PROJECT_NAME/
-DEPLOY_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy.log"
-DEPLOY_ERR_LOG_PATH="/home/ubuntu/$PROJECT_NAME/deploy_err.log"
-APPLICATION_LOG_PATH="/home/ubuntu/$PROJECT_NAME/application.log"
+JAR_PATH="/home/ec2-user/github_action/build/libs/*.jar"
+DEPLOY_PATH=/home/ec2-user/$PROJECT_NAME/
+DEPLOY_LOG_PATH="/home/ec2-user/$PROJECT_NAME/deploy.log"
+DEPLOY_ERR_LOG_PATH="/home/ec2-user/$PROJECT_NAME/deploy_err.log"
+APPLICATION_LOG_PATH="/home/ec2-user/$PROJECT_NAME/application.log"
 BUILD_JAR=$(ls $JAR_PATH)
 JAR_NAME=$(basename $BUILD_JAR)
 
